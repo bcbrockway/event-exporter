90e0ab72f3ba778e2d6e8d838dd8eea1702a7805
Nick <nbadger@mintel.com>
2019-02-14 12:24:58 +0000

    Remove uwanted debug

--- sinks/tcp.go
+++ sinks/tcp.go
@@ -176,11 +176,9 @@ func doTCPRequest(config *TCPConf, entries []*api_v1.Event) error {
 	}
 	defer conn.Close()
 
-
 	for _, data := range(entries) {
 		// Convert to json string
 		params, err := encodeData(data)
-		glog.Infof("Params %v", params.Bytes())
 		if err != nil	{
 			return err
 		}
